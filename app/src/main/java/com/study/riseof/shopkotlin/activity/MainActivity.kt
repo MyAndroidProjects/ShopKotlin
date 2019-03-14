b25ba30878bfa0d7c676a3ed68a751029762b758
MyAndroidProjects <riseofthecookies@yandex.ru>
2019-03-14 15:00:30 +0700

    Main layout, menu, colors, icons, etc.

--- app/src/main/java/com/study/riseof/shopkotlin/activity/MainActivity.kt
+++ app/src/main/java/com/study/riseof/shopkotlin/activity/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.study.riseof.shopkotlin
+package com.study.riseof.shopkotlin.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import com.study.riseof.shopkotlin.R
+import kotlinx.android.synthetic.main.coordinator_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
     }
 }
